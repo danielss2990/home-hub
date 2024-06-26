b91cdff42adfa06788975fb65262e8865dd6781f
Daniel Souza <daniel.souza@bit.com.br>
2024-06-26 15:40:39 -0300

    two micro rendering

--- root/src/home-hub-root-config.ts
+++ root/src/home-hub-root-config.ts
@@ -1,12 +1,18 @@
 import { registerApplication, start, LifeCycles } from "single-spa";
 
+// registerApplication({
+//   name: "@single-spa/welcome",
+//   app: () =>
+//     System.import<LifeCycles>(
+//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+//     ),
+//   activeWhen: ["/"],
+// });
+
 registerApplication({
-  name: "@single-spa/welcome",
-  app: () =>
-    System.import<LifeCycles>(
-      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-    ),
-  activeWhen: ["/"],
+  name: "@home-hub/hello-world",
+  app: () => System.import<LifeCycles>("@home-hub/hello-world"),
+  activeWhen: ["/"]
 });
 
 // registerApplication({
