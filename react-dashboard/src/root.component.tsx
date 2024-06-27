41b57597437c3439f87be0b1f8461aec92d64621
Daniel Souza <daniel.souza@bit.com.br>
2024-06-27 13:47:35 -0300

    cards

--- react-dashboard/src/root.component.tsx
+++ react-dashboard/src/root.component.tsx
@@ -1,3 +1,13 @@
-export default function Root(props) {
-  return <section>Hello World!</section>;
+import HeroCard from './components/HeroCard';
+import UsersCard from './components/UsersCard';
+import WaterCard from './components/WaterCard';
+
+export default function Root() {
+    return (
+        <div>
+            <HeroCard />
+            <UsersCard />
+            <WaterCard />
+        </div>
+    );
 }
--- root/src/home-hub-root-config.ts
+++ root/src/home-hub-root-config.ts
@@ -22,17 +22,16 @@ import { registerApplication, start, LifeCycles } from "single-spa";
 // });
 
 registerApplication({
-  name: '@home-hub/react-navbar',
-  app: () => System.import<LifeCycles>('@home-hub/react-navbar'),
-  activeWhen: ['/'],
+	name: '@home-hub/react-navbar',
+	app: () => System.import<LifeCycles>('@home-hub/react-navbar'),
+	activeWhen: (location) => location.pathname.includes('/'),
 });
 
 registerApplication({
-  name: '@home-hub/react-dashboard',
-  app: () => System.import<LifeCycles>('@home-hub/react-dashboard'),
-  activeWhen: ['/'],
+	name: '@home-hub/react-dashboard',
+	app: () => System.import<LifeCycles>('@home-hub/react-dashboard'),
+	activeWhen: (location) => location.pathname.includes('/'),
 });
-
 
 registerApplication({
   name: '@home-hub/light-control',
